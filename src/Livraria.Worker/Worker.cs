68dcf9a7bb483947009f05139b3ee7b3107919ad
ALMEIDA Walquiria Morgana Santana de (Consulting For Edenred) <walquiria.almeida@consulting-for.edenred.com>
2023-11-30 18:05:23 -0300

    initial

---
+++ src/Livraria.Worker/Worker.cs
@@ -0,0 +1,22 @@
+namespace Livraria.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Console.Out.WriteLineAsync("teste teste");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
