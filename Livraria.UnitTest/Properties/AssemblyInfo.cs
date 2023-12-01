beca8def93e26b724175e5660540ad7d6b61afb8
ALMEIDA Walquiria Morgana Santana de (Consulting For Edenred) <walquiria.almeida@consulting-for.edenred.com>
2023-12-01 15:34:26 -0300

    alterações + gitignore

---
+++ Livraria.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Livraria.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Edenred")]
+[assembly: AssemblyProduct("Livraria.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Edenred 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de59d027-1cc7-4946-8c1b-2f33d6e3492e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
