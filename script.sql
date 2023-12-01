b7cfeb3b0628f52bad518f7c959ec613762330ad
ALMEIDA Walquiria Morgana Santana de (Consulting For Edenred) <walquiria.almeida@consulting-for.edenred.com>
2023-12-01 14:35:19 -0300

    Adicionando script SQL

---
+++ script.sql
@@ -0,0 +1,39 @@
+
+-- Criar o banco de dados se não existir
+CREATE DATABASE IF NOT EXISTS livraria;
+USE livraria;
+
+-- Criar a tabela Produtos
+CREATE TABLE IF NOT EXISTS Produtos (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    valor DECIMAL(10, 2) NOT NULL,
+    imagem VARCHAR(255),
+    quantidade INT NOT NULL
+);
+
+select * from Produtos;
+
+-- Criar a tabela Carrinho
+CREATE TABLE IF NOT EXISTS Carrinho (
+    id INT PRIMARY KEY,
+    id_produto INT,
+    id_compra INT,
+    quantidade INT NOT NULL,
+    FOREIGN KEY (id_produto) REFERENCES Produtos(id)
+);
+
+select * from Carrinho;
+
+-- Criar a tabela Compra
+CREATE TABLE IF NOT EXISTS Compra (
+    id INT PRIMARY KEY,
+    id_carrinho INT,
+    valor_total DECIMAL(10, 2) NOT NULL,
+    Status VARCHAR(50),
+    Data_compra DATE,
+    FOREIGN KEY (id_carrinho) REFERENCES Carrinho(id)
+);
+
+select * from Compra;
+
